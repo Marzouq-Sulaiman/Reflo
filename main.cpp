5d97f56edff3e4ce749bb82231597fe0df3cf2a1
Marzouq <3marzouq@gmail.com>
2025-08-17 11:55:47 -0400

    update

--- main.cpp
+++ main.cpp
@@ -1,20 +1,4 @@
-#include <Thermistor.h>
-
-
-Thermistor therm1(0);
-
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
-  int temperature = int(thermo1.getTemp());
-  
-  Serial.println(temperature);
-  delay(1000);
-
 /**
- * TODO: Will be adding improved sensitivity before Myhal testing. See https://lastminuteengineers.com/esp32-basics-adc/ or Reflo Spreadsheet row 6
+ * ATTENTION: Do NOT add code to this file; add all "main" code that you wish to be run in the cpp file found in reflo_esp_32_firmware/src/main.cpp 
+ * This was the old main file before PlatformIO was configured. It will likely be removed in future
  */
-
-}
