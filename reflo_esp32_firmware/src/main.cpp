46c682bf0b383ceca469b9040233df9097fbd6c8
Zaeem-Ahmad <70673816+Zaeem-Ahmad@users.noreply.github.com>
2025-08-25 09:35:44 -0400

    renamed variables for easier identification

--- reflo_esp32_firmware/src/main.cpp
+++ reflo_esp32_firmware/src/main.cpp
@@ -1,48 +1,46 @@
 #include <Arduino.h>
 #define BAUD 9600
-#define VREF 3.3
 
 // global variables used a lot
 const double resolution = 4096.0;
 
 // Voltage Sensor init
 const int voltage_sensA = 32;
-float vout = 0.0;
-float vin = 0.0;
-float R1 = 30000.0;
-float R2 = 7500.0;
-double value = 0;
+float v_voutA = 0.0;
+float v_vinA = 0.0;
+float v_R1A = 30000.0;
+float v_R2A = 7500.0;
+double v_valueA = 0;
 
 // Current Sensor Init
 const int current_sensA = 33;
-double Vout = 0;
-double Current = 0;
-const double scale_factor = 0.066;
-const double vRef = 3.3;
-const double resConvert = 4096.0;
-double resADC = vRef / resConvert;
-double zeroPoint = vRef / 2;
+double i_voutA = 0;
+double currentA = 0;
+const double i_scale_factorA = 0.066;
+const double i_vRefA = 3.3;
+double i_resADC_A = i_vRefA / resolution;
+double zeroPoint = i_vRefA / 2;
 
 void read_current() {
   // Vout is read 1000 Times for precision
   for (int i = 0; i < 1000; i++) {
-    Vout = (Vout + (resADC * analogRead(current_sensA)));
+    i_voutA = (i_voutA + (i_resADC_A * analogRead(current_sensA)));
     delay(1);
   }
 
   // Get Vout in mv
-  Vout = Vout / 1000;
+  i_voutA = i_voutA / 1000;
 
   // Convert Vout into Current using Scale Factor
-  Current = (Vout - zeroPoint) / scale_factor;
+  currentA = (i_voutA - zeroPoint) / i_scale_factorA;
 
   // Print Vout and Current to two Current = ");
 
   Serial.print("Vout = ");
-  Serial.print(Vout, 2);
+  Serial.print(i_voutA, 2);
   Serial.print(" Volts");
   Serial.print("\t Current = ");
-  Serial.print(Current, 2);
+  Serial.print(currentA, 2);
   Serial.println(" Amps");
 
   delay(1000);
@@ -50,11 +48,11 @@ void read_current() {
 
 void read_voltage() {
   // read the value at analog input
-  value = analogRead(voltage_sensA);
-  vout = (value * 3.3) / resolution;  // see text
-  vin = vout / (R2 / (R1 + R2));
+  v_valueA = analogRead(voltage_sensA);
+  v_voutA = (v_valueA * 3.3) / resolution;  // see text
+  v_vinA = v_voutA / (v_R2A / (v_R1A + v_R2A));
   Serial.print("INPUT V= ");
-  Serial.println(vin, 2);
+  Serial.println(v_vinA, 2);
   delay(500);
 }
 
